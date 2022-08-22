120270271a1315ce8a6329278c0549933a1c4aa1
Victor-Murilo <victor2706.vm@gmail.com>
2022-08-22 15:08:37 -0300

    Resolução do requisito 16,17 e 18

--- src/pages/Foods/index.jsx
+++ src/pages/Foods/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Foods() {
   return (
     <div>
       Hello Word! estou no Foods
       <Header pageName="Foods" />
+      <Footer />
     </div>
   );
 }
