98c7ed153bf7e337e36a14af439e45b98a234485
SilasPerillo <silasperillo@gmail.com>
2022-08-24 14:46:26 -0300

    req 27

--- src/components/Recommendations/index.jsx
+++ src/components/Recommendations/index.jsx
@@ -5,34 +5,68 @@ import './style.css';
 
 export default function Recommendations({ typePage }) {
   const [recipesRecommender, setRecipesRecommender] = useState([]);
+  const [currentIndex1, setCurrentIndex1] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(1);
+  const [carousel, setCarousel] = useState([]);
+
   const imgUrl = typePage === 'drinks' ? 'strMealThumb' : 'strDrinkThumb';
   const title = typePage === 'drinks' ? 'strMeal' : 'strDrink';
 
   const invertTypePage = typePage === 'drinks' ? 'food' : 'drink';
   const SIX = 6;
 
+  const BLOCK = { display: 'block' };
+  const NONE = { display: 'none' };
+
+  const updateIndexCarousel = (next) => {
+    if (!next) {
+      setCurrentIndex1((currentIndex1 + recipesRecommender
+        .length - 1) % recipesRecommender.length);
+      setCurrentIndex2((currentIndex2 + recipesRecommender
+        .length - 1) % recipesRecommender.length);
+    }
+    if (next) {
+      setCurrentIndex1((currentIndex1 + 1) % recipesRecommender.length);
+      setCurrentIndex2((currentIndex2 + 1) % recipesRecommender.length);
+    }
+  };
+
   useEffect(() => {
     const getResults = async () => {
       const data = await getMealsOrDrinks(invertTypePage);
-      setRecipesRecommender(data.splice(0, SIX));
+      setRecipesRecommender(data.slice(0, SIX));
     };
     getResults();
   }, [invertTypePage]);
 
-  console.log(recipesRecommender);
-
   return (
     <div className="container-recommended">
-      { recipesRecommender.map((elem, index) => (
-        <div
-          className="card-recommended"
-          key={ index }
-          data-testid={ `${index}-recomendation-card` }
-        >
-          <span>{ elem[title] }</span>
-          <img className="img-recommended" src={ elem[imgUrl] } alt={ elem[title] } />
-        </div>
-      )) }
+      <button
+        name="prev"
+        type="button"
+        onClick={ () => updateIndexCarousel(false) }
+      >
+        Prev
+      </button>
+      { recipesRecommender
+        .map((elem, index) => (
+          <div
+            className="card-recommended"
+            key={ index }
+            style={ index === currentIndex1 || index === currentIndex2 ? BLOCK : NONE }
+            data-testid={ `${index}-recomendation-card` }
+          >
+            <p data-testid={ `${index}-recomendation-title` }>{ elem[title] }</p>
+            <img className="img-recommended" src={ elem[imgUrl] } alt={ elem[title] } />
+          </div>
+        )) }
+      <button
+        name="next"
+        type="button"
+        onClick={ () => updateIndexCarousel(true) }
+      >
+        Next
+      </button>
     </div>
   );
 }
