8fa7e7a3ed5396d52aa7ec460ec9a1ca0b6b77dc
David <david.lourenco94@gmail.com>
2022-08-25 14:44:53 -0300

    primeiro commit

--- src/pages/FavoriteRecipes/index.jsx
+++ src/pages/FavoriteRecipes/index.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 export default function FavoriteRecipes() {
   return (
     <div>
+      <p>Hello !</p>
       <Header pageName="Favorite Recipes" search={ false } />
     </div>
   );
